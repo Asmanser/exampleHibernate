892fb0f4f9554a970c19156d405f8ae106a8bffc
Alexander Kazimov <bybirmybir@gmail.com>
2019-02-11 16:53:11 +0300

    Models. Create simple model user.

---
+++ hibernate/src/main/java/by/andersen/training/hib/models/User.java
@@ -0,0 +1,39 @@
+package by.andersen.training.hib.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    private int age;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
