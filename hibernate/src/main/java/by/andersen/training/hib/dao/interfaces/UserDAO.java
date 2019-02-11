58e0db5b822a9327a8db053061749c5f4a2fbff6
Alexander Kazimov <bybirmybir@gmail.com>
2019-02-11 17:22:12 +0300

    Interfaces. Create interface UserDao with necessary methods

---
+++ hibernate/src/main/java/by/andersen/training/hib/dao/interfaces/UserDAO.java
@@ -0,0 +1,22 @@
+package by.andersen.training.hib.dao.interfaces;
+
+import by.andersen.training.hib.models.Auto;
+import by.andersen.training.hib.models.User;
+
+import java.util.List;
+
+public interface UserDAO {
+
+    public User findById(int id);
+
+    public void save(User user);
+
+    public void update(User user);
+
+    public void delete(User user);
+
+    public Auto findAutoById(int id);
+
+    public List<User> findAll();
+
+}
